e8e2fdc88a3062b0a7d3d46b695304c58ae682aa
José de Moura <jose_moura99@outlook.com>
2022-01-26 14:26:54 -0300

    Basic static components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,18 @@
+import DataTable from "components/DataTable";
+import Footer from "components/Footer";
+import NavBar from "components/NavBar";
+
 function App() {
   return (
-    <div className="text-primary">
-      Olá Mundo!
-    </div>
+    <>
+      <NavBar />
+
+      <div className="container">
+        <DataTable />
+      </div>
+
+      <Footer />
+    </>
   );
 }
 
