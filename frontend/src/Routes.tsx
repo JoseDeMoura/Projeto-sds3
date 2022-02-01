137446c82dcde6ee613bf6163afbe70c3801725b
José de Moura <jose_moura99@outlook.com>
2022-02-01 10:02:23 -0300

    Tentativa de voltar no versionamento

---
+++ frontend/src/Routes.tsx
@@ -0,0 +1,14 @@
+import { Switch } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
+
+const Routes = () => {
+    return (
+        <BrowserRouter>
+            <Switch>
+                
+            </Switch>
+        </BrowserRouter>
+    );
+}
+
+export default Routes;
