a732dc1bc923a1bd7cdb0feabc3977ba1b905399
José de Moura <jose_moura99@outlook.com>
2022-02-02 09:32:56 -0300

    DonutChart Integration

--- frontend/src/components/DonutChart/index.tsx
+++ frontend/src/components/DonutChart/index.tsx
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { useEffect, useState } from 'react';
 import Chart from 'react-apexcharts';
 import { SaleSum } from 'types/sale';
 import { BASE_URL } from 'utils/requests';
@@ -10,26 +11,43 @@ type ChartData = {
 
 const DonutChart = () => {
 
+    //useState -> Mantém o estado do componente.
+    const [chartData, setChartData] = useState<ChartData>({ labels: [], series: [] });
+
+    //useEffect -> Executa algo na instanciação ou destruição do componente, observa estado
+    useEffect(() => {
+        axios.get(`${BASE_URL}/sales/amount-by-seller`)
+            .then(response => {
+
+                const data = response.data as SaleSum[];
+                const mylabels = data.map(x => x.sellerName);
+                const myseries = data.map(x => x.sum);
+
+                setChartData({ labels: mylabels, series: myseries });
+            })
+    }, []);
+
+    //FORMA ERRADA       CHAMADA ASSÍNCRONA
+    //let chartData: ChartData = {labels: [], series: []};
+
     //FORMA ERRADA
-    let chartData: ChartData = {labels: [], series: []};
-
-    axios.get(`${BASE_URL}/sales/amount-by-seller`)
-    .then(response => {
-
-        const data = response.data as SaleSum[];
-        const mylabels = data.map(x => x.sellerName);
-        const myseries = data.map(x => x.sum);
-
-        chartData = {labels: mylabels, series: myseries}
-
-        console.log(chartData);
-    })
+    //axios.get(`${BASE_URL}/sales/amount-by-seller`)
+    //.then(response => {
+    //
+    //    const data = response.data as SaleSum[];
+    //    const mylabels = data.map(x => x.sellerName);
+    //    const myseries = data.map(x => x.sum);
+    //
+    //    setChartData({labels: mylabels, series: myseries});
+    //
+    //    console.log(chartData);
+    //})
 
     //const mockData = {
     //    series: [477138, 499928, 444867, 220426, 473088],
     //    labels: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
     //}
-    
+
     const options = {
         legend: {
             show: true
@@ -38,7 +56,7 @@ const DonutChart = () => {
 
     return (
         <Chart
-            options={{ ...options, labels: chartData.labels}}
+            options={{ ...options, labels: chartData.labels }}
             series={chartData.series}
             type="donut"
             height="240"
